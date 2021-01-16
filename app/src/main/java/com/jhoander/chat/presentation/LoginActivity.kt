be7adbb2c8597fb86633aa9d346bac919727d06c
jhoander <jhoander.torres@externos-cl.cencosud.com>
2021-01-16 10:48:55 -0300

    Se añade firebase y se construye actividad de login

---
+++ app/src/main/java/com/jhoander/chat/presentation/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.jhoander.chat.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jhoander.chat.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
